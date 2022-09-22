26a03e6e212d056656cc4f43b9537862806de11d
antwonlimbrick <101299738+antwonlimbrick@users.noreply.github.com>
2022-09-21 19:55:36 -0500

    Add files via upload

---
+++ Homework1/WarmUp.py
@@ -0,0 +1,25 @@
+
+print("Enter integer:")
+
+user_num = int(input())
+
+print("You entered:", user_num)
+
+num_squared = user_num * user_num
+num_cubed = user_num * user_num * user_num
+
+print(user_num, "squared is", num_squared)
+
+print("And",user_num,"cubed is",num_cubed,"!!")
+
+print("Enter another integer:")
+
+other_num = int(input())
+
+sum_ = user_num + other_num
+
+product = user_num * other_num
+
+print(user_num,"+",other_num, "is", sum_ )
+
+print(user_num,"*",other_num, "is", product )
