169ca96612b0efc81e9c96be23eb9d2c7c33e19a
antwonlimbrick <101299738+antwonlimbrick@users.noreply.github.com>
2022-10-14 22:09:46 -0500

    Add files via upload

---
+++ Homework2/Coding Problem 2.py
@@ -0,0 +1,48 @@
+#Antwon Limbrick
+#1972657
+
+
+def get_month(monthString):
+        if monthString == 'January':
+            month = 1
+        elif monthString == 'February':
+            month= 2
+        elif monthString == 'March':
+            month = 3
+        elif monthString == 'April':
+            month = 4
+        elif monthString == 'May':
+            month = 5
+        elif monthString == 'June':
+            month = 6
+        elif monthString == 'July':
+            month = 7
+        elif monthString == 'August':
+            month = 8
+        elif monthString == 'September':
+            month = 9
+        elif monthString == 'October':
+            month = 10
+        elif monthString == 'November':
+            month = 11
+        elif monthString == 'December':
+            month = 12
+        else:
+            month = 0
+        return month
+
+user_string = input()
+    
+if __name__ == '__main__':
+    string1=[]
+    string2=[]
+    while user_string != "-1":
+        string1.append(user_string)
+        user_string = input()
+    for i in string1:
+        if len(i.split()) > 1:
+            if ',' in i.split()[1]:
+                print('{}/{}/{}'.format(get_month(i.split()[0]), i.split()[1][:-1], i.split()[2]))
+                
+                
+
