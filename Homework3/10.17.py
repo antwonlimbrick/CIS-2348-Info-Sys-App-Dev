b577c38735c928aac320746e0d301377eddbbdb0
antwonlimbrick <101299738+antwonlimbrick@users.noreply.github.com>
2022-11-02 19:34:18 -0500

    Update 10.17.py

--- Homework3/10.17.py
+++ Homework3/10.17.py
@@ -2,10 +2,10 @@
 
 
 class ItemToPurchase:
-    def __init__(self):
-        self.item_name = str('none')
-        self.item_price = float(0)
-        self.item_quantity = int(0)
+    def __init__(self, item_name= 'none', item_price=0, item_quantity=0):
+        self.item_name = item_name
+        self.item_price = item_price
+        self.item_quantity = item_quantity
         
     def print_item_cost(self):
         print(self.item_name, end = ' ')
